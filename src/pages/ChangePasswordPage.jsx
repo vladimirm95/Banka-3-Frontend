1f4d3fcc5a256588d53871f7ca9619eb81c15882
Zorana Stefanović <147942333+zstefanovic141@users.noreply.github.com>
2026-03-19 14:41:18 +0100

    Update ChangePasswordPage.jsx
    Nepotreban kod

--- src/pages/ChangePasswordPage.jsx
+++ src/pages/ChangePasswordPage.jsx
@@ -10,11 +10,7 @@ export default function ChangePasswordPage() {
 
   const { token } = location.state || {};
 
-  // zaštita ako neko direktno uđe na stranicu
-   if (!token) {
-     navigate("/login");
-    return null;
-   }
+
 
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
